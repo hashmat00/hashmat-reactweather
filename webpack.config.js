751f4b27b5e9adfae00af9a2d1fe9bdcafbdda7e
hashmat00 <info@hashmat00.com>
2017-02-22 21:37:02 +0000

    added webpackconfig file, added entry, output src, and babel-loader for jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     
-    entry: './public/app.js',
+    entry: './public/app.jsx',
     output: {
         path: __dirname,
         filename: '/public/bundle.js'
@@ -8,6 +8,18 @@ module.exports = {
     
     resolve: {
         extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+              loader: 'babel-loader',
+              query: {
+                  presets: ['react', 'es2015']
+                   },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
     }
     
 };
